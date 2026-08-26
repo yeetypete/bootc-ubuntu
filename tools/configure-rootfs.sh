172fb3ef2e336ac89498e373184c10b6be0d5d4e
Peter Siegel <psiegel2000@icloud.com>
2026-08-26 19:26:59 +0200

    fix: keep /usr/local a regular directory as bootc recommends
    (cherry picked from commit 3a796d1b76218a4a7338e4eee8b2f138bcf8a9af)

--- tools/configure-rootfs.sh
+++ tools/configure-rootfs.sh
@@ -14,12 +14,11 @@ cp "/boot/vmlinuz-${kver}" "/usr/lib/modules/${kver}/vmlinuz"
 # the composefs digest sealed into the UKI excludes the UKI but not its
 # parent directory.
 # shellcheck disable=SC2114
-rm -rf /boot /srv /home /root /usr/local /mnt
+rm -rf /boot /srv /home /root /mnt
 mkdir -p /boot/EFI/Linux /sysroot /var
 ln -s /var/home /home
 ln -s /var/roothome /root
 ln -s /var/srv /srv
-ln -s /var/usrlocal /usr/local
 ln -s /var/mnt /mnt
 ln -s sysroot/ostree /ostree
 rm -f /etc/fstab
